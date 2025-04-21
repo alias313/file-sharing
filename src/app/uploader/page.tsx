4f0d6e9aab06df84fc1af251fed60cdba70099e6
Elies <eliesgarciaalvira@gmail.com>
2025-04-22 01:17:41 +0200

    Drag and drop works

--- src/app/uploader/page.tsx
+++ src/app/uploader/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { UploadButton } from "~/utils/uploadthing";
+import { FileDropzone } from "~/components/file-dropzone";
+import { useState } from "react";
 
 export default function Home() {
+  const [currentFile, setCurrentFile] = useState<File | null>(null);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UploadButton
@@ -17,6 +21,19 @@ export default function Home() {
           alert(`ERROR! ${error.message}`);
         }}
       />
+      <FileDropzone
+        acceptedFileTypes={["image/*", ".jpg", ".jpeg", ".png", ".webp", ".svg"]}
+        dropText="Drop image file"
+        setCurrentFile={(file) => setCurrentFile(file)}
+      >
+        <div className="mt-4 p-6 border border-gray-300 rounded-lg">
+          {currentFile ? (
+            <p>Selected file: {currentFile.name}</p>
+          ) : (
+            <p>Drag and drop an image here or click the upload button above</p>
+          )}
+        </div>
+      </FileDropzone>
     </main>
   );
 }
