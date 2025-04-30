44e6a85f75f1421dff0d649f95fdf28a55b712e8
Elies García Alvira <68759092+alias313@users.noreply.github.com>
2025-04-30 18:32:03 +0200

    Add audio file route and endpoint (#2)

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -54,6 +54,23 @@ export const ourFileRouter = {
       console.log("file url", file.ufsUrl);
       return { uploadedBy: metadata.userId };
     }),
+    audioUploader: f({
+      audio: {
+        maxFileSize: "4MB",
+        maxFileCount: 1,
+      },
+    })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+      if (!user) throw new UploadThingError("Unauthorized");
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.ufsUrl);
+      return { uploadedBy: metadata.userId };
+    }),
+
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
