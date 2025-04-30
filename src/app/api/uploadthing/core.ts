5bb950ba18242e98556f0c668f9038f595cde2fe
Elies García Alvira <68759092+alias313@users.noreply.github.com>
2025-04-30 17:43:18 +0200

    Split uploader route into two for videoUploader inclusion (#1)
    * Added video uploader
    
    * Two routes
    
    One for videos the other for images

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -38,6 +38,22 @@ export const ourFileRouter = {
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
     }),
+    videoUploader: f({
+      video: {
+        maxFileSize: "16MB",
+        maxFileCount: 1,
+      },
+    })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+      if (!user) throw new UploadThingError("Unauthorized");
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.ufsUrl);
+      return { uploadedBy: metadata.userId };
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
---
+++ src/app/video/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { UploadDropzone } from "~/utils/uploadthing";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-full max-w-2xl">
+        <UploadDropzone
+          endpoint="videoUploader"
+          onClientUploadComplete={(res) => {
+            console.log("Files: ", res);
+            alert("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            alert(`ERROR! ${error.message}`);
+          }}
+          onUploadBegin={(name) => {
+            console.log("Uploading: ", name);
+          }}
+          config={{
+            mode: "auto"
+          }}
+        />
+      </div>
+    </main>
+  );
+}
