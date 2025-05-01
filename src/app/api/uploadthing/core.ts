44f4657e3ab0241857b6a3d7d25052700080c0ef
Elies García Alvira <68759092+alias313@users.noreply.github.com>
2025-05-01 20:07:16 +0200

    Add PDF file route and endpoint (#3)
    * Added pdf api endpoint
    
    * Added route for uploading PDFs

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -70,6 +70,22 @@ export const ourFileRouter = {
       console.log("file url", file.ufsUrl);
       return { uploadedBy: metadata.userId };
     }),
+    pdfUploader: f({
+      pdf: {
+        maxFileSize: "4MB",
+        maxFileCount: 1,
+      },
+    })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+      if (!user) throw new UploadThingError("Unauthorized");
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.ufsUrl);
+      return { uploadedBy: metadata.userId };
+    }),
 
 } satisfies FileRouter;
 
