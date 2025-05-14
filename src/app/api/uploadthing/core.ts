ad6b864f9dd908e2694fc5f08c3acfc1954168fd
Elies García Alvira <68759092+alias313@users.noreply.github.com>
2025-05-14 08:56:35 +0200

    Add text endpoint and route (#4)
    * Add text endpoint and route
    
    * Forgot to change to text FileType

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -86,6 +86,22 @@ export const ourFileRouter = {
       console.log("file url", file.ufsUrl);
       return { uploadedBy: metadata.userId };
     }),
+    textUploader: f({
+      text: {
+        maxFileSize: "64KB",
+        maxFileCount: 1,
+      },
+    })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+      if (!user) throw new UploadThingError("Unauthorized");
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.ufsUrl);
+      return { uploadedBy: metadata.userId };
+    }),
 
 } satisfies FileRouter;
 
